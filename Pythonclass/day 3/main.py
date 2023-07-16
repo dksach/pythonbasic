35b9eb5d30a817ec44755ac99805677b03703aa7
dksach <96976802+dksach@users.noreply.github.com>
2023-07-16 15:18:06 +0545

    Add files via upload

---
+++ Pythonclass/day 3/main.py
@@ -0,0 +1,44 @@
+# data = [23,45,4548,'ram']
+# print(type(data))
+# print(dir(data))
+
+# data=[]
+# data.append('ram')
+# print(data)
+
+
+# data=[
+#     1,2,3,[[[45]]],78
+#     ]
+# print(data[3][0][0][0])
+# print(data[4]) 
+
+# data=['ram','sita','gita']
+# data1=['a','b']
+# data.extend(data1)
+# print(data)
+
+# data=[1,23]
+# data.insert(1,45)
+# print(data)
+
+# students=['ram','sita','gita','hari','shyam']
+# students.remove('hari')
+# print(students)
+
+# students=['ram','sita','gita','hari','shyam']
+# students.sort(reverse=True)
+# print(students)
+
+# data=[1,2,3,4,1,1,2,3]
+# print(data.count(1))
+
+# data = {
+#     'name':'ram',
+#     'age':10,
+#     'address':{
+#         'city':'ktm',
+#     }
+# }
+# print(data.get('names','notfound'))
+# print(data['address']['city'])
